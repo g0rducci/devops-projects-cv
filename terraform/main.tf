f8cd4249656a432685d0f7afb3884b61fe15e4fb
YairRodriguez96 <64326986+g0rducci@users.noreply.github.com>
2023-04-08 01:07:13 -0600

    update ami

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,7 +14,7 @@ provider "aws" {
   region = var.region
 }
 resource "aws_instance" "servernode" {
-  ami                    = "ami-0a695f0d95cefc163"
+  ami                    = "ami-007855ac798b5175e"
   instance_type          = "t2.micro"
   key_name               = aws_key_pair.deployer.key_name
   vpc_security_group_ids = [aws_security_group.maingroup.id]
